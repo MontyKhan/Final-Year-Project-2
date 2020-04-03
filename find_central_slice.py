11106944c7fbec8691d6b423350d03ea27b410e3
Robert Clark <robclark.wgc@gmail.com>
2020-04-03 18:54:49 +0100

    Script now checks if DICOM tags are present and correct.

--- find_central_slice.py
+++ find_central_slice.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import pydicom
 
 def str_join(*args):
     return ''.join(map(str, args))
@@ -10,18 +11,32 @@ output = 'D:\Central slices'
 for patient in os.listdir(dataset):
 	filename = os.fsdecode(patient)
 	filepath = os.path.join("D:\CT study Georgia Breen", filename)
+
 	if (os.path.isdir(filepath)) and filename.endswith('MRI'):
 		print(os.fsdecode(patient))
 		file_list = os.listdir(filepath)
+
 		if filename in file_list:
 			filepath = os.path.join(filepath, filename)
 			file_list = os.listdir(filepath)
+
+		dicom_list = []
+
 		for file in file_list:
 			if not (os.fsdecode(file).endswith('.dcm')):
-				file_list.remove(file)
-		median = int(len(file_list)/2)
+				continue
+			else:
+				dicom_file = os.path.join(filepath, os.fsdecode(file))
+				ds = pydicom.read_file(dicom_file)
+				if (ds.SeriesDescription == 'T2 SAG BRAIN'):
+					dicom_list.append(file)
+				else:
+					continue
+
+		median = int(len(dicom_list)/2) + 1
 		print(median)
-		central_slice = os.path.join(filepath, file_list[median])
+		print(dicom_list[median])
+		central_slice = os.path.join(filepath, dicom_list[median])
 		shutil.copy(central_slice, output)
 	else:
 		continue
