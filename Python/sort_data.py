f9d10eac6b921bd478c979626c58465736fa26aa
Robert Clark <robclark.wgc@gmail.com>
2020-04-09 20:34:08 +0100

    Added Python script to sort images into correct groups.

---
+++ Python/sort_data.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+def sort_data():
+
+	# Set path for directory containing files to be sorted
+	image_folder = 'D:\Coursework\Final-Year-Project-2\Central slices\Images (cropped)'
+	
+	# Set output directories
+	affected_folder = os.path.join(image_folder, 'affected')
+	control_folder = os.path.join(image_folder, 'control')
+
+	# Check output directories exist
+	if not (os.path.isdir(affected_folder)):
+		os.mkdir(affected_folder)
+	if not (os.path.isdir(control_folder)):
+		os.mkdir(control_folder)
+
+	# Move files to correct directories
+	for file in os.listdir(image_folder):
+		filename = os.fsdecode(file)
+		filepath = os.path.join(image_folder, filename)
+
+		if ('affected' in filename):
+			shutil.move(filepath, affected_folder)
+		elif ('control' in filename):
+			shutil.move(filepath, control_folder)
+
+	print('Finished')
+
+sort_data()
