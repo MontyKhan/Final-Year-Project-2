fe6897afed593899f785573f7aa3b1f0a33b16c3
Robert Clark <robclark.wgc@gmail.com>
2020-04-27 02:23:10 +0100

    Added script to produce 3D point cloud of MRI scan.

--- generateEdgeCloud3D.m
+++ generateEdgeCloud3D.m
@@ -2,32 +2,45 @@ function generateEdgeCloud3D(path)
 
 % Initial input
 input = dir(path);
-output = 'D:\Coursework\Final-Year-Project-2\Central slices\Skulls (edges)\';
+output = 'D:\Coursework\Final-Year-Project-2\MRIs\Edges\';
 
 % Get subfolders
 directories = [];
 for i = 3:length(input)
-    subfolder = strcat(path, string(input(i).name));
+    subfolder = strcat(path, '\', string(input(i).name), '\');
+    co_ords = [];
     if (input(i).isdir())
-        subfolder = strcat(subfolder, '\');
-        generateEdgeCloud(subfolder);
-    else
-        im = imread(subfolder);
-        im = rgb2gray(im);
-        [EdgeMag, EdgeDir] = imgradient(im);
+        subfolder = dir(subfolder);
         
-        % imshowpair(EdgeMag, EdgeDir, 'montage');
-        % imshow(EdgeMag, [min(EdgeMag(:)) max(EdgeMag(:))]);
-        EdgeMag = mat2gray(EdgeMag, [min(EdgeMag(:)) max(EdgeMag(:))]);
-        EdgeMag = (EdgeMag > 0.1);
+        for j = 3:length(subfolder)
+            filepath = strcat(subfolder(j).folder, '\', subfolder(j).name);
+            im = imread(filepath);
+            % im = rgb2gray(im);
+            [EdgeMag, EdgeDir] = imgradient(im);
+            
+            % imshowpair(EdgeMag, EdgeDir, 'montage');
+            % imshow(EdgeMag, [min(EdgeMag(:)) max(EdgeMag(:))]);
+            EdgeMag = mat2gray(EdgeMag, [min(EdgeMag(:)) max(EdgeMag(:))]);
+            EdgeMag = (EdgeMag > 0.1);
+            
+            fileName = erase(input(i).name,'.png');
+            % patientName = filename(1:(end-6));
+
+            output_patient = strcat(output, fileName, '\');
+
+            if ~(isfolder(output_patient))
+                mkdir(string(output_patient));
+            end
+
+            imwrite(EdgeMag,strcat(output_patient,fileName, '_', string(j-2), '.png'));
+
+            % Generate point cloud
+            [row, col] = find(EdgeMag);
+            co_ords = [co_ords ; [row, col, repmat((j-3)*2.7, size(row,1), 1)]];
+        end
         
-        patientName = erase(input(i).name,'.png');
-        imwrite(EdgeMag,strcat(output,patientName,'.png'));
-        
-        % Generate point cloud
-        [row, col] = find(EdgeMag);
-        pt_cloud = pointCloud([row, col, repmat(1,length(row),1)]);
-        filename_pc = strcat(output, 'Point clouds\', patientName);
+        pt_cloud = pointCloud(co_ords);
+        filename_pc = strcat(output, 'Point clouds\', fileName);
         pcwrite(pt_cloud,filename_pc,'PLYFormat','binary');
     end
     
